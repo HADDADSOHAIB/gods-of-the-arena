0d81a1b7d4b11d8a75204d98e6909bdaf269599a
HADDADSOHAIB <sohaib@hoggo.com>
2023-11-11 17:40:14 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Gladiator.create([
+  {
+    name: 'Spartacus',
+    age: 30,
+    attack_points: 80
+  },
+  {
+    name: 'Gannicus',
+    age: 35
+  },
+  {
+    name: 'Oenomaus',
+    age: 40,
+    attack_points: 90
+  },
+  {
+    name: 'Varro',
+    age: 20
+  },
+  {
+    name: 'Barca',
+    age: 20,
+    attack_points: 70
+  },
+])
